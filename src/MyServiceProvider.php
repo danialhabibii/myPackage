9f4b34362141246e35d88bd0090f66f1b1137655
Rospter <danialhbi02@gmail.com>
2023-11-02 10:20:28 +0330

    test

--- src/MyServiceProvider.php
+++ src/MyServiceProvider.php
@@ -9,6 +9,7 @@ class MyServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->publishes([__DIR__ . '/config' => config_path('test.php')]);
     }
 
     public function register()
